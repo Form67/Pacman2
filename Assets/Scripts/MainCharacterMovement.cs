48f7879d235ca550304bc89ab7fd57d72c90e288
Sam Gould <drmcman67@yahoo.com>
2018-11-14 00:03:35 -0500

    Movement 2- (DONT MERGE YET)

--- Assets/Scripts/MainCharacterMovement.cs
+++ Assets/Scripts/MainCharacterMovement.cs
@@ -15,22 +15,24 @@ public class MainCharacterMovement : MonoBehaviour {
     public enum Dir { up, down, left, right };
     public Dir direction;
     public float lerpCycle;
-
+    public Node currentNode;
+    public Node targetNode;
     public float invDurationPerPellet;
     [HideInInspector]
     public float invincibleTimer = 0f;
     public bool isInvincible = false;
-
+    protected PathFinding pathFinder;
     int ghostScore = 100;
 
     mapGenerator map;
     UIDisplay ui;
-
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         map = FindObjectOfType<mapGenerator>();
         ui = FindObjectOfType<UIDisplay>();
-
+        pathFinder = GameObject.FindGameObjectWithTag("pathfinding").GetComponent<PathFinding>();
+    }
     void Start () {
         //new code
     
@@ -47,83 +49,106 @@ public class MainCharacterMovement : MonoBehaviour {
     {
         //new code
 
-        lerpCycle += Time.deltaTime;
+        lerpCycle += Time.deltaTime*velocity;
         if (lerpCycle >= 1f) {
             //update grid
         }
-
-
-/*
-        scoreText.text = "Score: " + score;
-        if (score > highScore) {
-            highScore = score;
-            scoreText2.text = scoreText2.text = "Highscore: " + highScore;
-        }
-        if (!dead)
+        // Power pellets
+        if (isInvincible)
         {
-            // Power pellets
-            if(isInvincible)
-            {
-                invincibleTimer -= Time.deltaTime;
+            invincibleTimer -= Time.deltaTime;
 
-                if (invincibleTimer <= 0)
-                {
-                    isInvincible = false;
-                    invincibleTimer = 0;
-                    ghostScore = 100;
-                }
-            }
-
-            //input
-            if (Input.GetKeyDown(KeyCode.R))
+            if (invincibleTimer <= 0)
             {
-                SceneManager.LoadScene(0);
+                isInvincible = false;
+                invincibleTimer = 0;
+                ghostScore = 100;
             }
+        }
+        currentNode = pathFinder.WorldPosToNode(transform.position);
+        if (pathFinder.IsNodeIntersection(currentNode)&& lerpCycle <.51f) {
             if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             {
-                direction = -1;
+ 
                 currVelocity = velocity;
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
             {
-                direction = 1;
                 currVelocity = velocity;
                 transform.rotation = Quaternion.Euler(0, 0, 90);
             }
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             {
-                direction = -1;
                 currVelocity = -velocity;
                 transform.rotation = Quaternion.Euler(0, 0, 180);
             }
             if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             {
-                direction = 1;
                 currVelocity = -velocity;
                 transform.rotation = Quaternion.Euler(0, 0, 270);
             }
+        }
+        /*
+                scoreText.text = "Score: " + score;
+                if (score > highScore) {
+                    highScore = score;
+                    scoreText2.text = scoreText2.text = "Highscore: " + highScore;
+                }
+                if (!dead)
+                {
 
-            if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) < .1f && Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < .1f)
-            {
-                GetComponent<Animator>().SetBool("Moving", false);
-            }
-            else
-            {
-                GetComponent<Animator>().SetBool("Moving", true);
-            }
-            if (currVelocity != 0)
-            {
-                if (direction < 0)
-                    GetComponent<Rigidbody2D>().velocity = new Vector3(currVelocity, 0, 0);
-                else
-                    GetComponent<Rigidbody2D>().velocity = new Vector3(0, currVelocity, 0);
-            }
-            else
-            {
-                GetComponent<Animator>().SetBool("Moving", false);
-            }
-        }*/
+
+                    //input
+                    if (Input.GetKeyDown(KeyCode.R))
+                    {
+                        SceneManager.LoadScene(0);
+                    }
+                    if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+                    {
+                        direction = -1;
+                        currVelocity = velocity;
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                    }
+                    if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+                    {
+                        direction = 1;
+                        currVelocity = velocity;
+                        transform.rotation = Quaternion.Euler(0, 0, 90);
+                    }
+                    if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+                    {
+                        direction = -1;
+                        currVelocity = -velocity;
+                        transform.rotation = Quaternion.Euler(0, 0, 180);
+                    }
+                    if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+                    {
+                        direction = 1;
+                        currVelocity = -velocity;
+                        transform.rotation = Quaternion.Euler(0, 0, 270);
+                    }
+
+                    if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) < .1f && Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < .1f)
+                    {
+                        GetComponent<Animator>().SetBool("Moving", false);
+                    }
+                    else
+                    {
+                        GetComponent<Animator>().SetBool("Moving", true);
+                    }
+                    if (currVelocity != 0)
+                    {
+                        if (direction < 0)
+                            GetComponent<Rigidbody2D>().velocity = new Vector3(currVelocity, 0, 0);
+                        else
+                            GetComponent<Rigidbody2D>().velocity = new Vector3(0, currVelocity, 0);
+                    }
+                    else
+                    {
+                        GetComponent<Animator>().SetBool("Moving", false);
+                    }
+                }*/
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
